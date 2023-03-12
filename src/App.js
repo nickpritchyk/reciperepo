6c9511066049090598eead452676d6e6c6ae19f1
nickpritchyk <62455273+nickpritchyk@users.noreply.github.com>
2023-03-12 16:25:55 -0600

    styled recipe cards and search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
+import Favorites from './pages/favorites'
 import Navbar from './components/NavBar';
 import Footer from "./components/Footer";
 
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />}> </Route>
+          <Route path="/favorites" element={<Favorites />}> </Route>
         </Routes>
         <Footer />
       </Router>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import '../styles/SearchBar.css'
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import '../styles/SearchBar.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
+import { Link } from 'react-router-dom'
+
 
 export const SearchBar = () => {
     const [search, setSearch] = useState("")
@@ -22,24 +29,37 @@ export const SearchBar = () => {
 
 
     return (
-        <div className='input-wrapper'>
+        <Container>
+            <Row>
             <form className='search-div' onSubmit={handleSearch}>
                 <input type="text" placeholder="Search for recipes..." onChange={(e) =>  setSearch(e.target.value)}/>
+                <button className='search-btn'type='submit' onSubmit={handleSearch}> <FaSearch/></button>
+
             </form>
+            </Row>
+            <Row>
             {loading ? ( <h3>Loading...</h3>
             ) : (
-                results.map((item) => 
-                    <div className="card">
-                        <img className="card-img-top" src={item.image} alt="Recipe Pic"/>
-                    <div className="container">
-                        <h5 className="card-title">{item.title}</h5>
-                        <p className="card-text">Lorem Ipsum</p>
-                        <a href="www.google.com" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+                results.map((item) => {
+                    return(
+                            <Col xs={6} md={4}>
+                                <Card>
+                                    <Card.Img fluid className='img' variant="top" src={item.image}/>
+                                    <Card.Body>
+                                        <Card.Title>{item.title}</Card.Title>
+                                        <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                    )
+                }
                 )
               )}
-        </div>
+            </Row>
+        </Container>
     );
 }
 
