d0d19954c0292d2341eb5ddb183462e6a35e88d6
nickpritchyk <62455273+nickpritchyk@users.noreply.github.com>
2023-03-12 18:19:41 -0600

    cleaned up SearchBar component

---
+++ src/components/RecipeCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Card from "react-bootstrap/Card";
+
+const RecipeCard = ({results}) => {
+    return(
+        results.map((item) => {
+            return(
+        <Col xs={6} md={4}>
+            <Card className='card-hover'>
+                <Card.Img fluid className='img' variant="top" src={item.image} />
+                <Card.Body>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+            )
+        })
+    )
+}
+
+export default RecipeCard;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,9 +4,9 @@ import '../styles/SearchBar.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container'
 import { Link } from 'react-router-dom'
+import RecipeCard from './RecipeCard'
 
 
 export const SearchBar = () => {
@@ -17,7 +17,7 @@ export const SearchBar = () => {
     const handleSearch = async(e) => {
         e.preventDefault();
         setLoading(true);
-        const tester = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=63bbfa20bb0d42b8927bc25fe8795946&query=' + search + ''
+        // const tester = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=63bbfa20bb0d42b8927bc25fe8795946&query=' + search + ''
         fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=63bbfa20bb0d42b8927bc25fe8795946&query=' + search + '')
             .then(response => response.json())
             .then((json) => {
@@ -27,36 +27,18 @@ export const SearchBar = () => {
         });
     }
 
-
     return (
         <Container>
             <Row>
             <form className='search-div' onSubmit={handleSearch}>
                 <input type="text" placeholder="Search for recipes..." onChange={(e) =>  setSearch(e.target.value)}/>
                 <button className='search-btn'type='submit' onSubmit={handleSearch}> <FaSearch/></button>
-
             </form>
             </Row>
             <Row>
             {loading ? ( <h3>Loading...</h3>
             ) : (
-                results.map((item) => {
-                    return(
-                            <Col xs={6} md={4}>
-                                <Card>
-                                    <Card.Img fluid className='img' variant="top" src={item.image}/>
-                                    <Card.Body>
-                                        <Card.Title>{item.title}</Card.Title>
-                                        <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                    )
-                }
-                )
+                <RecipeCard results={results}/>
               )}
             </Row>
         </Container>
